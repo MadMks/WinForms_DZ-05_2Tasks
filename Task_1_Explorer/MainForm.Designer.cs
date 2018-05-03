33ce19fef26598ff3cdd86f5a1b574930aec32c8
MadMks <mks59k@gmail.com>
2018-05-03 08:06:45 +0300

    1

--- Task_1_Explorer/MainForm.Designer.cs
+++ Task_1_Explorer/MainForm.Designer.cs
@@ -28,20 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.listView = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 72);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(152, 180);
+            this.treeView.TabIndex = 0;
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(170, 72);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(305, 180);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 264);
+            this.ClientSize = new System.Drawing.Size(487, 264);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.treeView);
             this.Name = "MainForm";
             this.Text = "Проводник";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
     }
 }
 
--- Task_1_Explorer/MainForm.cs
+++ Task_1_Explorer/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,59 @@ namespace Task_1_Explorer
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            TreeNode node;
+
+            foreach (DriveInfo item in DriveInfo.GetDrives())
+            {
+                if (item.IsReady == true)
+                {
+                    node = new TreeNode(item.Name);
+
+                    this.treeView.Nodes.Add(node);
+                }
+            }
+
+
+            //TreeNode node1 = new TreeNode("1");
+
+            this.AddFoldersRecursively(this.treeView.Nodes);
+
+            //this.treeView.Nodes.Add(node1);
+
+            foreach (TreeNode item in this.treeView.Nodes)
+            {
+
+            }
+        }
+
+        private void AddFoldersRecursively(TreeNodeCollection nodes)
+        {
+            TreeNode node;
+
+            foreach (TreeNode item1 in nodes)
+            {
+                foreach (string item2 in Directory.EnumerateDirectories(item1.FullPath))
+                {
+                    node = new TreeNode(item2);
+
+                    item1.Nodes.Add(node);
+                }
+
+                foreach (TreeNode item4 in item1.Nodes)
+                {
+                    foreach (string item3 in Directory.EnumerateDirectories(item4.FullPath))
+                    {
+                        node = new TreeNode(item3);
+
+                        item4.Nodes.Add(node);
+                    }
+                }
+                
+            }
         }
     }
 }
